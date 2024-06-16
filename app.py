2a4662906841d94746995d97c5e12485e2e933e9
rohitpaul351 <77479707+rohitpaul351@users.noreply.github.com>
2024-06-17 01:41:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -219,5 +219,5 @@ def thank_you():
     name = request.args.get('name')
     return f'Thank you, {name}, for your message!'
 
-# if __name__ == "__main__":
-#     app.run(debug=True, host="0.0.0.0")
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0")
